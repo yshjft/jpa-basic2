411512476380ad5053f0bb95421fe63cdc706e72
hong yuseok <jericoh@naver.com>
2021-08-22 14:36:51 +0900

    first commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,51 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setName("member1");
+            member.setTeam(team);
+            em.persist(member);
+
+            em.flush();
+            em.clear ();
+
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+            System.out.println(members.size());
+
+            for(Member m : members) {
+                System.out.println("m = " + m.getName());
+            }
+
+
+            tx.commit();
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+
+    }
+}
